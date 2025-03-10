bd02ef51e59a882704274e281f9ca34f16963b0d
“Archie <2898047h@student.gla.ac.uk>
2025-03-10 21:10:33 +0000

    Added registration templates

--- app/models.py
+++ app/models.py
@@ -4,14 +4,11 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_id = models.CharField(max_length=10, unique=True)
     website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
     description = models.CharField(max_length=200, blank=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, unique=True)
-
-    # to add preferences
 
     def __str__(self):
         return self.user.username
@@ -25,4 +22,5 @@ class Match(models.Model):
     user1_status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("accepted", "Accepted"), ("declined", "Declined")])
     user2_status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("accepted", "Accepted"), ("declined", "Declined")])
 
-    
+    def __str__(self):
+        return self.user1.username+" "+self.user1_status+", "+self.user2.username+" "+self.user2_status
--- dinner_with_a_stranger/urls.py
+++ dinner_with_a_stranger/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 urlpatterns = [
     path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
-    #path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
