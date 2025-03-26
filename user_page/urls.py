d19b4ca30c4bb6606a9df9cbf42465a9e324fdb8
“Archie <2898047h@student.gla.ac.uk>
2025-03-26 18:48:52 +0000

    Edited various things to do with registration

--- user_page/urls.py
+++ user_page/urls.py
@@ -6,6 +6,5 @@ app_name = 'userpage'
 urlpatterns = [
     path('me/', views.profile_home, name='profile_home'),
     path('me/edit/', views.edit_profile, name='edit_profile'),
-    path('me/change-password/', views.change_password, name='change_password'),
     path('<int:user_id>/', views.view_profile, name='view_profile'),
 ]
