2b18e2015866536a94dfea1cdd5a33b9d7c818c6
“Archie <2898047h@student.gla.ac.uk>
2025-03-27 12:36:18 +0000

    Removed non-existant urls from urls.py

--- matches/urls.py
+++ matches/urls.py
@@ -4,8 +4,6 @@ from matches import views
 app_name = 'matches'
 
 urlpatterns = [
-    path('', views.match_list, name='match_list'),
-    path('<str:match_id>/', views.match_detail, name='match_detail'),
     path('pending/', views.matches_pending, name='matches_pending'),
     path('accepted/', views.matches_accepted, name='matches_accepted'),
     path('denied/', views.matches_denied, name='matches_denied'),
