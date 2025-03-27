d2ee79192237360123044e2a0a3d8d3ed5f04fb3
“Archie <2898047h@student.gla.ac.uk>
2025-03-27 12:28:24 +0000

    Rebased changes and refactored profile_home()

--- user_page/views.py
+++ user_page/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-<<<<<<< HEAD
-from django.contrib.auth.views import PasswordChangeView
-from django.contrib.auth.models import User  
-from .models import UserProfile
-from .forms import ProfileEditForm
 
-=======
+from django.contrib.auth.views import PasswordChangeView
+
+from django.contrib.auth.models import User  
+from matches.models import Match
+from .models import UserProfile
 from django.db.models import Q
 
-from matches.models import Match
-from user_page.models import UserProfile
-from django.contrib.auth.models import User
-
-from user_page.forms import EditProfileForm  # Import the form you just created
->>>>>>> 68f07454a925b6106a0ec99b5bd32b3cc08eb5f9
+from .forms import EditProfileForm
 
 @login_required
 def profile_home(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    return render(request, 'userpage/user_profile.html', {'profile': profile, 'is_own_profile': True})
+    
+    return render(request, 'userpage/user_profile.html', {
+            'view_user': request.user,
+            'profile': profile,
+            'is_own_profile': True,
+            'allow_view':True
+        })
 
 @login_required
 def edit_profile(request):
@@ -37,28 +37,18 @@ def edit_profile(request):
 
     return render(request, 'userpage/user_profile_edit.html', {'form': form})
 
-<<<<<<< HEAD
 class CustomPasswordChangeView(PasswordChangeView):
     template_name = 'userpage/change_password.html'
     success_url = '/profile/'  
 
-=======
 # View another user's profile by username
 @login_required
->>>>>>> 68f07454a925b6106a0ec99b5bd32b3cc08eb5f9
 def view_profile(request, username):
     user = get_object_or_404(User, username=username)
     profile = get_object_or_404(UserProfile, user=user)
 
-<<<<<<< HEAD
     is_own_profile = request.user == user
 
-    return render(request, 'userpage/user_profile.html', {
-        'profile': profile,
-        'is_own_profile': is_own_profile,
-        'view_user': user  
-    })
-=======
     try:
         match=Match.objects.get(Q(user1=request.user,user2=user) | Q(user2=request.user,user1=user))
         
@@ -70,10 +60,10 @@ def view_profile(request, username):
         allow_view=False
     finally:
 
-        return render(request, 'userpage/view_profile.html', {
+        return render(request, 'userpage/user_profile.html', {
             'view_user': user,
             'profile': profile,
+            'is_own_profile': is_own_profile,
             'allow_view':allow_view
         })
     
->>>>>>> 68f07454a925b6106a0ec99b5bd32b3cc08eb5f9
