cfebdbddcc51262251e6e213c0d2ffcc352bf4a7
darinockkkk <darynadariy@gmail.com>
2025-03-27 23:29:01 +0200

    fixing the /profile/profile issue

--- user_page/urls.py
+++ user_page/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'user_page'
 
 urlpatterns = [
-    path('', views.profile_home, name='profile_home'),
+    path('/', views.profile_home, name='profile_home'),
     path('edit/', views.edit_profile, name='edit_profile'),
-    path('me/',views.view_profile, name='view_profile'),
     path('<username>/', views.view_profile, name='view_profile'),
 ]
