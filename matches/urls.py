c259275a852b91e38ae23b4a3d4ae6bf7a701ceb
Hiba Baig <2875483b@student.gla.ac.uk>
2025-03-27 20:33:32 +0000

    Add comment in matches/urls.py to check commit

--- matches/urls.py
+++ matches/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 from matches import views
-from .views import total_matches, user_match_counts
-
-
 
 app_name = 'matches'
 
@@ -11,12 +8,10 @@ urlpatterns = [
     path('accepted/', views.matches_accepted, name='matches_accepted'),
     path('denied/', views.matches_denied, name='matches_denied'),
     path('possible/', views.matches_possible, name='matches_possible'),
+    path('block-confirm/', views.block_confirm, name='block_confirm'),
+    path('unmatch-confirm/', views.unmatch_confirm, name='unmatch_confirm'),
     path('base/', views.matches_base, name='matches_base'),
     path('update/<str:match_id>/<str:decision>/', views.update_match_status, name='update_match_status'),
-    path('ajax/pending/', views.ajax_matches_pending, name='ajax_matches_pending'),
-    path('<str:match_id>/confirm/<str:action_type>/', views.match_action_confirm, name='match_action_confirm'),
-    path('api/total-matches/', total_matches, name='total-matches'),
-    path('api/user-match-counts/', user_match_counts, name='user-match-counts'),
-    path('registration/preferences/', views.registration_preferences, name='registration_preferences'),
-
 ]
+
+#checking if I can commit 
