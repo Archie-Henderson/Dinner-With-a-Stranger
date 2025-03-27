206cba29aae891cb50adf200fd2168e18758143b
darinockkkk <darynadariy@gmail.com>
2025-03-27 14:46:33 +0200

    modified index page and added age field to user model

--- user_page/models.py
+++ user_page/models.py
@@ -8,6 +8,7 @@ class UserProfile(models.Model):
     description = models.CharField(max_length=200, blank=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, unique=True)
+    age = models.IntegerField()
 
     def __str__(self):
         return self.user.username
