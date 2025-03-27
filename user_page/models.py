f7e43142a982fab9a8d4734da3aa215615cd9ba6
“Archie <2898047h@student.gla.ac.uk>
2025-03-27 14:51:31 +0000

    Added min and max value to age and made migrations

--- user_page/models.py
+++ user_page/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,7 +9,7 @@ class UserProfile(models.Model):
     description = models.CharField(max_length=200, blank=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, unique=True)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(16), MaxValueValidator(100)])
 
     def __str__(self):
         return self.user.username
